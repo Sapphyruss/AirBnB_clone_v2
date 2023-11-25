f74dfd3eff063b5890fce52d2670f6f3011b345d
Sapphyruss <lugi.franklin@outlook.fr>
2023-11-25 09:53:03 -0800

    .

--- models/user.py
+++ models/user.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from models.place import Place
-from models.review import Review
-from sqlalchemy import Column,  Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
+import os
+import models
 
 
 class User(BaseModel, Base):
@@ -16,12 +17,16 @@ class User(BaseModel, Base):
         last_name: last name
     """
 
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='user',
-                          cascade='all, delete-orphan')
-    reviews = relationship('Review', backref='user',
-                           cascade='all, delete-orphan')
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", cascade='delete', backref="user")
+        reviews = relationship("Review", cascade='delete', backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
