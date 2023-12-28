9cd827089736492e824a52fa6fd4d49298cb5462
Sapphyruss <lugi.franklin@outlook.fr>
2023-12-28 04:58:01 -0800

    .

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""use storage for fetching data from the storage engine"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def end_session(exception):
+    """remove the current SQLAlchemy"""
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def List_of_states():
+    """display HTML page"""
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
